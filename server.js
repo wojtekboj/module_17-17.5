b399f06c2cbc92a0cf50667d822a63c99de21cd2
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-07-25 20:20:26 +0200

    Add project files

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('assets'));
+
+app.use(function(req, res, next){
+    console.log('Hej, jestem pośrednikiem między żądaniem a odpowiedzią!');
+    next();
+});
+
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.sendFile('/index.html');
+});
+
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
+});
+
+app.get('/store', function (req, res) {
+    res.end('To jest sklep');
+});
+
+var server = app.listen(3000, 'localhost', function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
